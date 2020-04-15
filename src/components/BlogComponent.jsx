904626c0066003d3a5c6c13c2f998fcea079c342
Theepan Kanan <63585695+Theepan97@users.noreply.github.com>
2020-04-16 04:55:37 +0530

    Fix/font awesome and media (#2)
    * fix: Responsive, medium on hom ,home banner
    
    * fix: fontAwesomeIconUpdateAndMediaComponent
    
    Co-authored-by: TriptaSingh <si.tripta@gmail.com>

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import BlogMediumComponent from './BlogMediumComponent';
-import '../styles/Blog.scss';
-import '../styles/Site.scss';
-import data from '../static/staticData.json';
+import React from "react";
+import BlogMediumComponent from "./BlogMediumComponent";
+import "../styles/Blog.scss";
+import "../styles/Site.scss";
+import data from "../static/staticData.json";
 
 export class BlogComponent extends React.Component {
-
-    componentDidMount() {
+  componentDidMount() {
     //     pageSize = 4;
-
     // var pageCount = $(".card").length / pageSize;
-
     // for (var i = 0; i < pageCount; i++) {
     //   $("#pagin").append(
     //     `<li class="page-item"><a class="page-link" href="#">${i + 1}</a></li> `
@@ -23,97 +20,100 @@ export class BlogComponent extends React.Component {
     //     if (n >= pageSize * (page - 1) && n < pageSize * page) $(this).show();
     //   });
     // };
-
     // showPage(1);
-
     // $("#pagin li").click(function() {
     //   $("#pagin li").removeClass("active");
     //   $(this).addClass("active");
     //   showPage(parseInt($(this).text()));
     //   return false;
     // });
-    }
-    render() {
-        return (
-            <section className="blog_area single-post-area p_120">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8 posts-list">
-                            <div className="single-post row">
-                                <div className="col-lg-12">
-                                    <div className="jumbotron text-center">
-                                        <div id="logo"></div>
-                                        <h1 className="display-4 mb-3 name name-small name-small-small">
-                                            {data.Blog1}
-                      </h1>
-                                        <div className="main_title">
-                                            <p>
-                                                {data.Blog2}
-                        </p>
-                                        </div>
-                                        <a
-                                            className="banner_btn"
-                                            href="https://medium.com/@sabesan96"
-                                            target="_blank"
-                                            role="button"
-                                        >{data.Blog3} <i className="fa fa-newspaper-o"></i
-                                        ></a>
-                                    </div>
-                                    <BlogMediumComponent />
-
-                                    <ul id="pagin" className="pagination justify-content-center"></ul>
-
-                                    <div className="text-center">
-                                        <div className="mb-5">
-                                            <a className="banner_btn" href="https://medium.com/@sabesan96"
-                                            >{data.Blog3} <i className="fa fa-newspaper-o"></i
-                                            ></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4">
-
-                            <div className="blog_right_sidebar">
-                                <aside className="single_sidebar_widget author_widget">
-                                    <a
-                                        className="twitter-timeline"
-                                        href="https://twitter.com/TheSabesan?ref_src=twsrc%5Etfw"
-                                        data-tweet-limit="3"
-                                    >{data.Blog4}</a
-                                    >
-                                </aside>
-                                <aside className="single_sidebar_widget popular_post_widget">
-                                    <div className="blogAside1">
-                                        <a
-                                            href="https://twitter.com/intent/tweet?screen_name=TheSabesan&ref_src=twsrc%5Etfw"
-                                            className="twitter-mention-button"
-                                            data-size="large"
-                                            data-dnt="true"
-                                            data-show-count="false"
-                                        >{data.Blog5}</a
-                                        >
-                                    </div>
-                                </aside>
-                                <div className="br"></div>
-                                <aside className="single_sidebar_widget author_widget">
-                                    <div className="googleCalendar">
-                                        <iframe
-                                            src="https://calendar.google.com/calendar/b/0/embed?height=225&amp;wkst=1&amp;bgcolor=%23fbfaff&amp;ctz=Asia%2FColombo&amp;src=c2FiZXNhbjk5NkBnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=bzg2cmN1YmJnbHN0OTQ3YnNrOHVnajNoMWt1b20wMWVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=ZW4ubGsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=c2FiZXNhbi4xNkBjc2UubXJ0LmFjLmxr&amp;color=%23009688&amp;color=%234285F4&amp;color=%23C0CA33&amp;color=%23D81B60&amp;color=%23009688&amp;showTitle=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0"
-                                            width="300"
-                                            height="225"
-                                            frameborder="0"
-                                            scrolling="no"
-                                        ></iframe>
-                                    </div>
-                                </aside>
-                            </div>
-                        </div>
+  }
+  render() {
+    return (
+      <section className="blog_area single-post-area p_120">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 posts-list">
+              <div className="single-post row">
+                <div className="col-lg-12">
+                  <div className="jumbotron text-center">
+                    <div id="logo"></div>
+                    <h1 className="display-4 mb-3 name name-small name-small-small">
+                      {data.Blog1}
+                    </h1>
+                    <div className="main_title">
+                      <p>{data.Blog2}</p>
                     </div>
-                </div>
-            </section>
-        )
-    }
+                    <a
+                      className="banner_btn"
+                      href="https://medium.com/@sabesan96"
+                      target="_blank"
+                      role="button"
+                    >
+                      {data.Blog3} <i className="fas fa-newspaper-o"></i>
+                    </a>
+                  </div>
+                  <BlogMediumComponent />
 
+                  <ul
+                    id="pagin"
+                    className="pagination justify-content-center"
+                  ></ul>
+
+                  <div className="text-center">
+                    <div className="mb-5">
+                      <a
+                        className="banner_btn"
+                        href="https://medium.com/@sabesan96"
+                      >
+                        {data.Blog3} <i className="fas fa-newspaper-o"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="blog_right_sidebar">
+                <aside className="single_sidebar_widget author_widget">
+                  <a
+                    className="twitter-timeline"
+                    href="https://twitter.com/TheSabesan?ref_src=twsrc%5Etfw"
+                    data-tweet-limit="3"
+                  >
+                    {data.Blog4}
+                  </a>
+                </aside>
+                <aside className="single_sidebar_widget popular_post_widget">
+                  <div className="blogAside1">
+                    <a
+                      href="https://twitter.com/intent/tweet?screen_name=TheSabesan&ref_src=twsrc%5Etfw"
+                      className="twitter-mention-button"
+                      data-size="large"
+                      data-dnt="true"
+                      data-show-count="false"
+                    >
+                      {data.Blog5}
+                    </a>
+                  </div>
+                </aside>
+                <div className="br"></div>
+                <aside className="single_sidebar_widget author_widget">
+                  <div className="googleCalendar">
+                    <iframe
+                      src="https://calendar.google.com/calendar/b/0/embed?height=225&amp;wkst=1&amp;bgcolor=%23fbfaff&amp;ctz=Asia%2FColombo&amp;src=c2FiZXNhbjk5NkBnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=bzg2cmN1YmJnbHN0OTQ3YnNrOHVnajNoMWt1b20wMWVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=ZW4ubGsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=c2FiZXNhbi4xNkBjc2UubXJ0LmFjLmxr&amp;color=%23009688&amp;color=%234285F4&amp;color=%23C0CA33&amp;color=%23D81B60&amp;color=%23009688&amp;showTitle=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0"
+                      width="300"
+                      height="225"
+                      frameBorder="0"
+                      scrolling="no"
+                    ></iframe>
+                  </div>
+                </aside>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
--- src/components/ProjectComponents/NurseryMSComponent.jsx
+++ src/components/ProjectComponents/NurseryMSComponent.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Nursery from '../../images/Nursery.jpg';
-import data from '../../static/staticData.json';
+import React from "react";
+import Nursery from "../../images/Nursery.jpg";
+import data from "../../static/staticData.json";
 export const NurseryMSComponent = () => {
-    return(
-        <section className="portfolio_details_area p_120">
+  return (
+    <section className="portfolio_details_area p_120">
       <div className="container">
         <div className="portfolio_details_inner">
           <div className="row">
@@ -16,40 +16,48 @@ export const NurseryMSComponent = () => {
               <div className="portfolio_right_text">
                 <h4>{data.NMS}</h4>
                 <p>
-                  {data.NMST1}<sup><font color="#777777">{data.rd}</font></sup>
+                  {data.NMST1}
+                  <sup>
+                    <font color="#777777">{data.rd}</font>
+                  </sup>
                   {data.NMST2}
                 </p>
                 <ul className="list">
-                  <li><span>{data.Year}</span>: {data.numYR}</li>
                   <li>
-                    <span><h5>{data.ToolsUsed}</h5></span>
+                    <span>{data.Year}</span>: {data.numYR}
                   </li>
-                  <li><span>{data.Software}</span>{data.NMST3}</li>
+                  <li>
+                    <span>
+                      <h5>{data.ToolsUsed}</h5>
+                    </span>
+                  </li>
+                  <li>
+                    <span>{data.Software}</span>
+                    {data.NMST3}
+                  </li>
                   <li></li>
                   <li>
-                    <span><h5>{data.Project}</h5></span>{data.ChatbotT4}
+                    <span>
+                      <h5>{data.Project}</h5>
+                    </span>
+                    {data.ChatbotT4}
                   </li>
                   <a
                     href="https://github.com/sabesansathananthan/Nursery_Management"
                     target="_blank"
                   >
                     <font color="black">
-                      <i
-                        className="fa fa-github-square customSize"
-                      ></i
-                    ></font>
+                      <i className="fab fa-github-square customSize"></i>
+                    </font>
                   </a>
                 </ul>
               </div>
             </div>
           </div>
-          <p>{data.NMST4}
-          </p>
-          <p>
-          {data.NMST5}
-          </p>
+          <p>{data.NMST4}</p>
+          <p>{data.NMST5}</p>
         </div>
       </div>
     </section>
-    )
-}
+  );
+};
