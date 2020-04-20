d9726bc838813b5b2991659200eda9481f3efcd0
Theepan Kanan <63585695+Theepan97@users.noreply.github.com>
2020-04-20 19:06:23 +0530

    add: BlogCard (#6)

--- src/components/ProjectCategoryComponent.jsx
+++ src/components/ProjectCategoryComponent.jsx
@@ -3,10 +3,9 @@ import React from "react";
 export const ProjectCategoryComponent = (props) => {
   return (
     <li data-filter={props.val}>
-      <a href="javascript:void(0);" id={props.val}>
+      <a href="javaScript:void(0);" id={props.val}>
         {props.val}
       </a>
     </li>
   );
 };
-
