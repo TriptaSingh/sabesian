9949bf01124a07f2564268705f75a5254cb3acb4
Theepan97 <Theepankan97@gmail.com>
2020-04-27 14:04:43 +0530

    fixed: hamburgerLink

--- src/Layout/Components/Header.jsx
+++ src/Layout/Components/Header.jsx
@@ -42,7 +42,7 @@ export default class HeaderComponent extends React.Component {
           </div>
           <nav>
             <div className="nav-mobile">
-              <a id="nav-toggle" href="#!">
+              <a id="nav-toggle" href="javaScript:void(0);">
                 <span></span>
               </a>
             </div>
